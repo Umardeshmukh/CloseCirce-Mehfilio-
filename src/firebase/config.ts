e17da145934de9270f5d2e2212833d63ceb835fd
Mohammad Umar Deshmukh <umardesh@gmail.com>
2025-12-02 17:56:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5060861969-c8170",
+  "appId": "1:773117651266:web:d1428b999c8c354526c0db",
+  "apiKey": "AIzaSyBJhmLFBSVBUUS2GRgG5jKZuGP679__XnU",
+  "authDomain": "studio-5060861969-c8170.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "773117651266"
+};
